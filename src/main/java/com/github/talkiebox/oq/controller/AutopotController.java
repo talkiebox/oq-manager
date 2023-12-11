6692097589dcc55c2c8e9ff5a5dc592428e4a93c
khlee <ringorainy@gmail.com>
2023-12-12 04:04:30 +0900

    autopotCon

--- src/main/java/com/github/talkiebox/oq/controller/AutopotController.java
+++ src/main/java/com/github/talkiebox/oq/controller/AutopotController.java
@@ -1,10 +1,23 @@
 package com.github.talkiebox.oq.controller;
 
+import com.github.talkiebox.oq.domain.entity.UserAccount;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "/autopot")
 @RestController
+@RequiredArgsConstructor
 public class AutopotController {
+
+    @PostMapping(value = "/login")
+    public UserAccount login(@RequestBody UserAccount userAccount) {
+        System.out.println(userAccount.getLoginId());
+        System.out.println(userAccount.getPassword());
+
+        return userAccount;
+    }
 
 }
